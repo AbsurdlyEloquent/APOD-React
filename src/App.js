684625cbffff3b8d4dea50e57fb110091cd00900
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-27 16:25:04 -0400

    redoing app.js structure

--- src/App.js
+++ src/App.js
@@ -9,8 +9,31 @@ class App extends Component {
       data: []
     }
   }
+
   componentDidMount() {
-    fethc
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&count=20`)
+      .then(returned => returned.json())
+      .then(returned => {this.setState({data: returned})});
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <header>
+          <Nav />
+          <h1>Amazing Spectacular Astronomy Photos</h1>
+          <h5>Now in React</h5>
+          <SearchBar />
+        </header>
+        <main>
+          {this.state.data.map(item => {
+            return (
+              <Item key={item.date} title={item.title} explan={item.explanation} url={item.url} hdurl={item.hdurl}/>
+            )
+          })}
+        </main>
+      </div>
+    )
   }
 }
 
