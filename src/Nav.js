55ac97f3c8dd7c3077ce53faae1a50d5f63f99d6
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-28 03:26:15 -0400

    converted Nav to function

--- src/Nav.js
+++ src/Nav.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react'
 import './Nav.css'
 
-class Nav extends Component {
-  today = () => {
-    this.props.today()
-  }
-  scrollTop = () => {
-    this.props.scrollTop()
-  }
-  render() {
-    return (
-      <nav>
-        <ul className="list">
-          <li onClick={this.today}>Today</li>
-          <a href="https://apod.nasa.gov/apod/">APOD</a>
-          <a href="https://github.com/AbsurdlyEloquent/apod-react">Source</a>
-          <li onClick={this.scrollTop}>Top</li>
-        </ul>
-      </nav>
-    )
-  }
+function Nav(props) {
+  return (
+    <nav>
+      <ul className="list">
+        <li onClick={props.today}>Today</li>
+        <a href="https://apod.nasa.gov/apod/">APOD</a>
+        <a href="https://github.com/AbsurdlyEloquent/apod-react">Source</a>
+        <li onClick={props.scrollTop}>Top</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Nav
